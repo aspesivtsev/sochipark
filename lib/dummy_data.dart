4d43f38d06678dd88e2f61186cc924a399de5e86
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-03-29 00:06:36 +0300

    adding models cont

---
+++ lib/dummy_data.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './models/attractions.dart';
+import './models/types.dart';
+
+const DUMMY_TYPES = const [
+  Type(
+    id: 't1',
+    title: 'Экстремальный',
+    color: Colors.red,
+  ),
+  Type(
+    id: 't2',
+    title: 'Семейный',
+    color: Colors.green,
+  ),
+  Type(
+    id: 't3',
+    title: 'Детский',
+    color: Colors.yellow,
+  ),
+  Type(
+    id: 't4',
+    title: 'Водный',
+    color: Colors.blue,
+  ),
+];
+
+const DUMMY_ATTRACTIONS = const [
+Attraction {
+  id:
+  title;
+  imageUrl;
+  description;
+  types;
+  workTime;
+  age;
+  withAdults;
+  isPurchasedSeparately;
+  isActive;
+];
+
+//https://www.youtube.com/watch?v=ScINEHxsVuw
--- lib/models/attractions.dart
+++ lib/models/attractions.dart
@@ -1,39 +1,32 @@
-enum Complexity { Simple, Challenging, Hard }
-
-enum Affordability {
-  Affordable,
-  Pricey,
-  Luxurious,
-}
-
 class Attraction {
   final String id;
-  final List<String> types;
   final String title;
   final String imageUrl;
-  final List<String> ingredients;
-  final List<String> steps;
-  final int duration;
-  final Complexity complexity;
-  final Affordability affordability;
-  final bool isGlutenFree;
-  final bool isLactoseFree;
-  final bool isVegan;
-  final bool isVegetarian;
+  final String description;
+  final List<String> types;
+  final String workTime;
+  final int age;
+  final String withAdults;
+  final bool isPurchasedSeparately;
+  final bool isActive;
 
   const Attraction({
     required this.id,
-    required this.types,
     required this.title,
     required this.imageUrl,
-    required this.ingredients,
-    required this.steps,
-    required this.duration,
-    required this.complexity,
-    required this.affordability,
-    required this.isGlutenFree,
-    required this.isLactoseFree,
-    required this.isVegan,
-    required this.isVegetarian,
+    required this.description,
+    required this.types,
+    required this.workTime,
+    required this.age,
+    required this.withAdults,
+    required this.isPurchasedSeparately,
+    required this.isActive,
   });
 }
+
+/*final Complexity complexity;
+  final Affordability affordability;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+
+  final bool isVegetarian;*/
