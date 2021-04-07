ad6c73e967564fdaa4ac3634d4523479b9bd2dea
anatoly <anspesiv@yahoo.com>
2021-04-07 21:12:54 +0300

    implementing a wave cont

--- lib/widgets/wave_widget.dart
+++ lib/widgets/wave_widget.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
-class WaveWidget extends StatelessWidget {
+class WaveWidget extends StatefulWidget {
   final Size size;
   final double yOffset;
   final Color color;
 
   WaveWidget({required this.size, required this.yOffset, required this.color});
+
+  @override
+  _WaveWidgetState createState() => _WaveWidgetState();
+}
+
+class _WaveWidgetState extends State<WaveWidget> with TickerProviderStateMixin {
+  AnimationController animationController;
+  List<Offset> wavePoints = [];
+  @override
+  void initState() {
+    super.initState();
+    animationController(vsync: this, duration: Duration(milliseconds: 5000))
+      ..addListener(() {
+        //wavePoints.clear();
+
+        final double waveSpeed = animationController.value * 1080;
+        final double fullSphere = animationController.value * Math.pi * 2;
+        final double normalizer = Math.cos(fullSphere);
+        final double waveWidth = Math.pi / 270;
+        final double waveHeight = 20.0;
+
+        for (int i = 0; i <= widget.size.width.toInt(); ++i) {
+          double calc = Math.sin((waveSpeed - i) * waveWidth);
+          wavePoints.add(
+            Offset(
+              i.toDouble(), //X
+              calc * waveHeight * normalizer + widget.yOffset, //Y
+            ),
+          );
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
